59db618649117262330dce2003c400c42e5cb463
A. Skomra <aaron.skomra@wacom.com>
2022-02-15 15:19:13 -0500

    update build script

--- build.sh
+++ build.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
 #Make wacom-gui rpm release from the master for the latest version
+VERSION=0.3.1
+RELEASE=0
+RELEASE_STRING=wacom-gui-$VERSION-$RELEASE
 
 #Assume your current location is ~
 sudo yum install git vim rpm-build make gcc 
+mkdir ~/rpmbuild
+mkdir ~/rpmbuild/SOURCES
 cd ~
 cp libwacom-1.11.tar.bz2 /home/aas/rpmbuild/SOURCES/
-rm -rf wacom-gui-0.3.1-rc1*
-cp -r wacom-gui wacom-gui-0.3.1-rc1
+rm -rf wacom-gui-*
+cp -r wacom-gui $RELEASE_STRING
 cp wacom-gui/rpmbuild/wacom-gui.spec ~/rpmbuild/wacom-gui.spec
-tar -czvf wacom-gui-0.3.1-rc1.tar.xz wacom-gui-0.3.1-rc1
-cp wacom-gui-0.3.1-rc1.tar.xz ~/rpmbuild/SOURCES
+tar -czvf $RELEASE_STRING.tar.xz $RELEASE_STRING
+cp $RELEASE_STRING.tar.xz ~/rpmbuild/SOURCES
 rpmbuild --target noarch -bb rpmbuild/wacom-gui.spec
 sudo yum remove wacom-gui -y
-sudo yum install --skip-broken ./rpmbuild/RPMS/noarch/wacom-gui-0.3.1-rc1.noarch.rpm -y
+sudo yum install --skip-broken ./rpmbuild/RPMS/noarch/$RELEASE_STRING.noarch.rpm -y
